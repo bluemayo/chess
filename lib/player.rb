8b932b6942c03d20ed4c146e1c66278142f616e3
Dylan <152871121+bluemayo@users.noreply.github.com>
2024-05-14 18:48:48 +0800

    Added User input verification

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+require_relative 'display'
+
 # This class defines a player in a chess game
 class Player
+  include Display
+
   def initialize(name)
     @name = name
     @pieces = []
@@ -13,14 +17,15 @@ class Player
   end
 
   def choose_piece
-    # TODO: Make sure user can only input the positions from a1 to h8
-    # TODO: Make it so user can't select pieces thats not theirs
     print 'Select Piece: '
-    check_move
+    move = check_move
+    return move if owned?(move)
+
+    display_not_owned
+    choose_piece
   end
 
   def move_to
-    # TODO: Make sure user can only input the positions from a1 to h8
     print 'Move to: '
     check_move
   end
@@ -28,14 +33,31 @@ class Player
   private
 
   def check_move
-    move = gets.chomp
-    to_row_and_column(move)
+    move = to_row_and_column(gets.chomp)
+    return move if in_range?(move)
+
+    display_invalid
+    check_move
   end
 
   def to_row_and_column(file_and_rank)
+    return [0, 0] unless file_and_rank.length == 2
+
     array = file_and_rank.split('').reverse
     array[0] = array[0].to_i
     array[1] = array[1].downcase.ord - 'a'.ord + 1
     array
+  end
+
+  def in_range?(position)
+    position.each { |each| return false if each < 1 || each > 8 }
+    true
+  end
+
+  def owned?(position)
+    @pieces.each do |piece|
+      return true if piece.instance_variable_get(:@position) == position
+    end
+    false
   end
 end
